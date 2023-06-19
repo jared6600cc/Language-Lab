4fde24ad23be6d57996d13ff1572cdac5a832391
jared6600cc <135299193+jared6600cc@users.noreply.github.com>
2023-06-19 16:34:14 -0500

    Added C# language 'echoBack'

---
+++ 0-helloWorld/echoBack.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace echoBack {
+    class EchoBack {
+        static void Main(string[] args)
+        {
+            /* Get string */
+            int MAXSTRINGLENGTH = 8;
+            string inString;
+            Console.WriteLine("Enter string: ");
+            inString = Console.ReadLine();
+            if (!string.IsNullOrEmpty(inString)) { 
+                while (true) {
+                    /* Test string */
+                    if (inString.Length > MAXSTRINGLENGTH) {
+                        // Entered string is too long
+                        Console.WriteLine("Exceeded max string length [{0}]\nEnter string: ", MAXSTRINGLENGTH);
+                        inString = Console.ReadLine();
+                    } else {
+                        Console.WriteLine("[ Entered: {0} ]", inString);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
